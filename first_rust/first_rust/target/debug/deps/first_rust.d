2744196b75c4467e427e61b6d58bde238dc29109
Paul Soteropulos <complexsimplicity22@yahoo.com>
2024-04-29 18:37:54 -0500

    setup

---
+++ first_rust/first_rust/target/debug/deps/first_rust.d
@@ -0,0 +1,5 @@
+C:\Users\compl\Desktop\Rust\first_rust\first_rust\target\debug\deps\first_rust.exe: src\main.rs
+
+C:\Users\compl\Desktop\Rust\first_rust\first_rust\target\debug\deps\first_rust.d: src\main.rs
+
+src\main.rs:
---
+++ first_rust/first_rust/target/debug/first_rust.d
@@ -0,0 +1,1 @@
+C:\Users\compl\Desktop\Rust\first_rust\first_rust\target\debug\first_rust.exe: C:\Users\compl\Desktop\Rust\first_rust\first_rust\src\main.rs
