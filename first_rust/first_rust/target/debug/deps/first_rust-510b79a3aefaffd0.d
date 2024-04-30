ec50f8db27b7d2220414ac42d6a72427b7197ebe
Paul Soteropulos <complexsimplicity22@yahoo.com>
2024-04-29 23:37:32 -0500

    idfk

---
+++ first_rust/first_rust/target/debug/deps/first_rust-510b79a3aefaffd0.d
@@ -0,0 +1,5 @@
+C:\Users\compl\Desktop\Rust\first_rust\first_rust\target\debug\deps\libfirst_rust-510b79a3aefaffd0.rmeta: src\main.rs
+
+C:\Users\compl\Desktop\Rust\first_rust\first_rust\target\debug\deps\first_rust-510b79a3aefaffd0.d: src\main.rs
+
+src\main.rs:
---
+++ first_rust/first_rust/target/debug/deps/first_rust-dde1da2219bf7c8c.d
@@ -0,0 +1,5 @@
+C:\Users\compl\Desktop\Rust\first_rust\first_rust\target\debug\deps\libfirst_rust-dde1da2219bf7c8c.rmeta: src\main.rs
+
+C:\Users\compl\Desktop\Rust\first_rust\first_rust\target\debug\deps\first_rust-dde1da2219bf7c8c.d: src\main.rs
+
+src\main.rs:
