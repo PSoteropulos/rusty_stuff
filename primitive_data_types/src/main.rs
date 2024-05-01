f70e626d430f84a5b775c5ef0231432c4ab69bcb
Paul Soteropulos <complexsimplicity22@yahoo.com>
2024-04-30 21:31:05 -0500

    primitive compile

--- primitive_data_types/src/main.rs
+++ primitive_data_types/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    //* Unsigned integers
+    let unsigned_num: u8 = 5; //u16, u32, u64, u128 (more bits = more numbers)
+
+    //* Signed integers
+    let signed_num: i8 = 5; //i16, i32, i64, i128
+
+    //* Floating point numbers
+    let float_num: f32 = 5.0; //f64
+
+    //* Platform specific integers
+    let arch_1: usize = 5;
+    let arch_2: isize = 5;
+
+    //* Characters
+    let char: char = 'a'; // character value must be in SINGLE QUOTES!!
+
+    //* Boolean
+    let b: bool = true;
 }
