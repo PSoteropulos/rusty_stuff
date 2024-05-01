497c266f64f20b3a14454ee52272e8979015be11
Paul Soteropulos <complexsimplicity22@yahoo.com>
2024-04-30 23:04:10 -0500

    compounds

--- compound_data_types/src/main.rs
+++ compound_data_types/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    //* &str and String */
+    let _fixed_str = "Fixed length String";
+    let mut _flexible_str = String::from("This string will grow");
+
+    //* Arrays
+    let mut _array_1 = [4, 5, 6, 8, 9];
+
+    //* Vectors
+    // let _vec0: Vec<i32> = vec![4, 5, 6, 8, 9];
+    type MyVector = Vec<i32>;
+    let _vec1: MyVector = vec![4, 5, 6, 8, 9];
+
+    //* Tuples
+    let my_info = (String::from("Salary"), 40000, "Age", 40);
+    let _salary_value = my_info.1;
+    let (_salary, _salary_value, _age, _age_value) = my_info;
 }
